bb7d1d14cc3530671c352a29096e51b3f0b6a3c1
Trevor Jacob Bickmore <b1ckmor3@rome.cs.byu.edu>
2020-02-21 18:19:12 -0700

    nothing

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("SubmitButton").addEventListener("click", (x) => {
+    e.preventDefault();
+    const originalCurrencyAmount = document.getElementById("ExchangeAmount").nodeValue;
+    const originalCurrencyType = document.getElementById("fromCurrency").onselect;
+    
+});
