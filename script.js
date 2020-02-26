6e380b95711dc2135e385edac78b0041b3fd59ff
Trevor Jacob Bickmore <b1ckmor3@sudan.cs.byu.edu>
2020-02-25 19:59:06 -0700

    The API is now working.
    The API is returning a response and then() returns the json. Using that
    json I've got the conversion working correctly, as well as a backwards
    conversion. Now just need to update the date and time, then make the
    website look nice.

--- script.js
+++ script.js
@@ -1,33 +1,104 @@
 const url = "https://api.exchangerate-api.com/v4/latest/";
 
-document.getElementById("SubmitButton").addEventListener("click", (e) => {
-    e.preventDefault();
-    const originalCurrencyAmount = parseInt(document.getElementById("ExchangeAmount").value);
-    const originalCurrencyType = document.getElementById("fromCurrency").value;
-    
-});
-
-document.getElementById("ExchangeAmount").addEventListener("keypress", (e) => {
-    //e.preventDefault();
-    const originalCurrencyAmount = parseInt(document.getElementById("ExchangeAmount").value);
+document.getElementById("ExchangeAmount").addEventListener("keyup", (e) => {
+    const originalCurrencyAmount = parseFloat(document.getElementById("ExchangeAmount").value);
     let fromCurrency = document.getElementById("fromCurrency").value; 
     let toCurrency = document.getElementById("toCurrency").value;
     if( fromCurrency === "default" || toCurrency === "default") {
         return;
     }
-    fetch(url + fromCurrency)
+    let fetchUrl = url + fromCurrency;
+    fetch(fetchUrl)
       .then((response) => {
-          let exchangeRate = 0;
-          for(let rate of response.json.rates) {
-              if (rate === toCurrency) {
-                  exchangeRate = rate;
-                  break;
-              }
-          } 
+          return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+          let exchangeRate = extractRate(toCurrency, json.rates);
           document.getElementById("ExchangeAmount2").value = originalCurrencyAmount * exchangeRate;
       })
       .catch((response) => {
-          console.log(response.json);
+          console.log(response);
+      });
+});
+
+document.getElementById("ExchangeAmount2").addEventListener("keyup", (e) => {
+    const originalCurrencyAmount = parseFloat(document.getElementById("ExchangeAmount2").value);
+    let fromCurrency = document.getElementById("toCurrency").value; 
+    let toCurrency = document.getElementById("fromCurrency").value;
+    if( fromCurrency === "default" || toCurrency === "default") {
+        return;
+    }
+    let fetchUrl = url + fromCurrency;
+    fetch(fetchUrl)
+      .then((response) => {
+          return response.json();
       })
-    
-})
+      .then((json) => {
+        console.log(json);
+          let exchangeRate = extractRate(toCurrency, json.rates);
+          document.getElementById("ExchangeAmount").value = originalCurrencyAmount * exchangeRate;
+      })
+      .catch((response) => {
+          console.log(response);
+      });
+});
+
+document.getElementById("toCurrency").addEventListener("change", (e) => {
+    const originalCurrencyAmount = parseFloat(document.getElementById("ExchangeAmount").value);
+    let fromCurrency = document.getElementById("fromCurrency").value; 
+    let toCurrency = document.getElementById("toCurrency").value;
+    if(originalCurrencyAmount === 'NaN' || fromCurrency === "default" || toCurrency === "default") {
+        return;
+    }
+    let fetchUrl = url + fromCurrency;
+    fetch(fetchUrl)
+      .then((response) => {
+          return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+          let exchangeRate = extractRate(toCurrency, json.rates);
+          document.getElementById("ExchangeAmount2").value = originalCurrencyAmount * exchangeRate;
+      })
+      .catch((response) => {
+          console.log(response);
+      });
+});
+
+document.getElementById("fromCurrency").addEventListener("change", (e) => {
+    const originalCurrencyAmount = parseFloat(document.getElementById("ExchangeAmount").value);
+    let fromCurrency = document.getElementById("fromCurrency").value; 
+    let toCurrency = document.getElementById("toCurrency").value;
+    if(originalCurrencyAmount === 'NaN' || fromCurrency === "default" || toCurrency === "default") {
+        return;
+    }
+    let fetchUrl = url + fromCurrency;
+    fetch(fetchUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        let exchangeRate = extractRate(toCurrency, json.rates);
+        document.getElementById("ExchangeAmount2").value = originalCurrencyAmount * exchangeRate;
+      })
+      .catch((response) => {
+        console.log(response);
+      });
+});
+
+function extractRate(currencyName, rates) {
+    for(let rate of Object.keys(rates)) {
+        if (rate === currencyName) {
+            return rates[rate];
+        }
+    }
+    return 'NaN';
+}
+
+function updateDateAndTime(json) {
+    let dateRoot = document.getElementById("date");
+    let timeRoot = document.getElementById("time");
+
+}
