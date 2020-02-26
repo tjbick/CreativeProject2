ca164015062b4ca67c5e3006a8e3a3ddc1a4fd46
Trevor Jacob Bickmore <b1ckmor3@australia.cs.byu.edu>
2020-02-26 15:18:28 -0700

    Finished, put in all the api options, added the footer.

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.getElementById("toCurrency").addEventListener("change", (e) => {
     const originalCurrencyAmount = parseFloat(document.getElementById("ExchangeAmount").value);
     let fromCurrency = document.getElementById("fromCurrency").value; 
     let toCurrency = document.getElementById("toCurrency").value;
-    if(originalCurrencyAmount === 'NaN' || fromCurrency === "default" || toCurrency === "default") {
+    if(originalCurrencyAmount === NaN || fromCurrency === "default" || toCurrency === "default") {
         return;
     }
     let fetchUrl = url + fromCurrency;
@@ -73,7 +73,7 @@ document.getElementById("fromCurrency").addEventListener("change", (e) => {
     const originalCurrencyAmount = parseFloat(document.getElementById("ExchangeAmount").value);
     let fromCurrency = document.getElementById("fromCurrency").value; 
     let toCurrency = document.getElementById("toCurrency").value;
-    if(originalCurrencyAmount === 'NaN' || fromCurrency === "default" || toCurrency === "default") {
+    if(originalCurrencyAmount === NaN || fromCurrency === "default" || toCurrency === "default") {
         return;
     }
     let fetchUrl = url + fromCurrency;
@@ -121,5 +121,30 @@ function updateDateAndTime(json) {
     result += '<div id="time">As of ' + time + ' </div>';
     result += '<div id="date">on ' + date.toDateString() + '</div>';
     document.getElementById("info-box").innerHTML = result;
-
 }
+
+const threeLetterCurrencyCodes = { "default": "Default", "AED": "UAE Dirham", "ARS": "Argentine Peso", "AUD": "Australian Dollar",
+    "BGN": "Bulgarian Lev", "BRL": "Brazilian Real", "BSD": "Bahamian Dollar", "CAD": "Canadian Dollar",
+    "CHF": "Swiss Franc", "CLP": "Chilean Peso", "CNY": "Chinese Renminbi", "COP": "Colombian Peso",
+    "CZK": "Czech Koruna", "DKK": "Danish Krone", "DOP": "Dominican Peso", "EGP": "Egyptian Pound",
+    "EUR": "Euro", "FJD":"Fiji Dollar", "GBP": "Pound Sterling", "GTQ": "Guatemalan Quetzal", 
+    "HKD": "Hong Kong Dollar","HRK": "Croatian Kuna", "HUF": "Hungarian Forint", "IDR": "Indonesian Rupiah",
+    "ILS": "Israeli Shekel", "INR": "Indian Rupee", "ISK": "Icelandic Krona", "JPY": "Japanese Yen", "KRW": "South Korean Won",
+    "KZT": "Kazakhstani Tenge", "MXN": "Mexican Peso", "MYR": "Malaysian Ringgit", "NOK": "Norwegian Krone",	
+    "NZD": "New Zealand Dollar", "PAB": "Panamanian Balboa", "PEN": "Peruvian Nuevo Sol", "PHP": "Philippine Peso",
+    "PKR": "Pakistani Rupee", "PLN": "Polish Zloty", "PYG": "Paraguayan Guarani", "RON": "Romanian Leu",
+    "RUB": "Russian Ruble", "SAR": "Saudi Riyal", "SEK": "Swedish Krona", "SGD": "Singapore Dollar", 
+    "THB": "Thai Baht", "TRY": "Turkish Lira", "TWD": "New Taiwan Dollar", "UAH": "Ukrainian Hryvnia",
+    "USD": "US Dollar", "UYU": "Uruguayan Peso", "ZAR": "South African Rand"}
+
+function writeOptions() {
+    var results;
+    for(let key of Object.keys(threeLetterCurrencyCodes)) {
+        results += '<option value="' + key + '">';
+        results += threeLetterCurrencyCodes[key];
+        results += '</option>';
+    }
+    document.getElementById("fromCurrency").innerHTML = results;
+    document.getElementById("toCurrency").innerHTML = results;
+}
+window.onload = writeOptions;
